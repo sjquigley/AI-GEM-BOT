5bd78ff75c0ec5a61136a7e8af0801383c56a743
Stephen Quigley <54909194+sjquigley@users.noreply.github.com>
2025-10-01 19:25:38 -0400

    Create api.rb

---
+++ api.rb
@@ -0,0 +1,34 @@
+# api.rb
+
+require 'sinatra'
+require 'sinatra/cross_origin' # Gem for handling web browser security
+require 'json'
+require 'my_custom_gemini_gem' # Your actual gem
+
+# Configure Cross-Origin Resource Sharing (CORS)
+# This is crucial to allow your website to talk to your API
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+# Define a POST endpoint because the website will be sending data (a prompt)
+post '/ask_gemini' do
+  # Get the JSON data sent from the website
+  request_payload = JSON.parse(request.body.read)
+  prompt = request_payload['prompt']
+
+  # Return an error if the prompt is missing
+  return { error: 'Prompt is missing' }.to_json if prompt.nil?
+
+  # Use your gem to get a response from the Google Gemini API
+  # The method name 'ask' is just an example
+  gemini_response = MyCustomGeminiGem.ask(prompt)
+
+  # Send the response back to the website as JSON
+  content_type :json
+  { response: gemini_response }.to_json
+end
